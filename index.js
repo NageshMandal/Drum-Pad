be09ea371bf1182126c7c6c4e0146c59fd59d0d5
NageshMandal <106006006+NageshMandal@users.noreply.github.com>
2022-09-29 14:37:14 +0530

    Update index.js

---
+++ index.js
@@ -0,0 +1,9 @@
+var drumButton = document.querySelectorAll(".drum").length;
+
+for (var i = 0; i<drumButton;i++){
+
+    document.querySelectorAll(".drum")[i].addEventListener("click",function(){
+
+    });
+
+}
